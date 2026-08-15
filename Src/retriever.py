55328978b243613a23b216b6e5292def492b5f2d
Chandan Nafria <chandannafria@gmail.com>
2026-08-15 13:04:37 +0530

    file change the code in retriever.py and to use mmr, it is better result

--- Src/retriever.py
+++ Src/retriever.py
@@ -13,9 +13,11 @@ class Retriever():
         try:
             logging.info("Creating Retriever...")
             retriever = self.vector_db.as_retriever(
-                search_type="similarity",
+                search_type="mmr",
                 search_kwargs ={
-                    "k" : 2
+                    "k" : 2,
+                    "fetch_k": 15,
+                    "lambda_mult": 0.5
                 }
             )
             logging.info("Retrieve created")
@@ -26,4 +28,19 @@ class Retriever():
         
         
         except Exception as e:
-            raise CustomException (e, sys)        
+            raise CustomException (e, sys)
+        
+    
+    def retrieve_documents(self, query):
+        try:
+            logging.info(f"retrieving documents for query : {query}")
+            retriever = self.get_retriever()
+            documents = retriever.invoke(query)
+            
+            logging.info(f"{len(documents)} documents retrieved")
+            
+            return documents
+        
+        
+        except Exception as e:
+            raise CustomException(e, sys)
